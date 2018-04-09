5fca3b601cde9475d44715e806cacf09decbe37d
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2018-04-09 15:19:18 +0700

    attribute dan method

--- src/praktek03/Handphone.java
+++ src/praktek03/Handphone.java
@@ -2,5 +2,11 @@
 package praktek03;
 
 public class Handphone extends Smartphone{
+    private String MerkHandphone;
+    
+    
+    void throttle(){
+        System.out.println("TUT.......TUT.......TUT");
+    }
     
 }
