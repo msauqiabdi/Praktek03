a1436b42b4fe2a3fb883ed293a7374b6161f565c
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2018-04-09 13:09:03 +0700

    buat getter dan setter

--- src/praktek03/Smartphone.java
+++ src/praktek03/Smartphone.java
@@ -11,5 +11,39 @@ public class Smartphone {
                 "Merk \t: "+merk+"\n"+
                 "Warna \t: "+warna+"\n"+
                 "Harga \t: "+harga);
-    }   
+    } 
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
