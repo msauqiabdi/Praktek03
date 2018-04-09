6e008d006e2173a498be4adec356a8ce8c25fbeb
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2018-04-09 13:03:21 +0700

    Instance, buat atribut dan panggil method

--- src/praktek03/SmartphoneAksi.java
+++ src/praktek03/SmartphoneAksi.java
@@ -1,6 +1,12 @@
 package praktek03;
 public class SmartphoneAksi {
     public static void main (String[] args) {
+        Smartphone s = new Smartphone();
+        s.os="Android";
+        s.merk="Samsung S9";
+        s.warna="Silver";
+        s.harga=5000000;
         
+        s.cetakInfo();
     }
 }
